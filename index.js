b8a38219113f0fa221f7fccaeb19e99592e3170c
Errorbot1122 <629329@lkstevens.wednet.edu>
2021-10-28 14:01:57 +0000

    Saving

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-const discordJS = require("discord.js")
+const DiscordJS, { Intents } = require("discord.js")
 const dotenv = require('dotenv')
 dotenv.config()
 
-const client = new discordJS.Client({
+const config = require('config.js')
+
+const cmds = {
+  'help': {
+    syntax: [
+      { name: 'command', optianal: false, desc: 'The command' }
+    ],
+    decription: 'Get the info of eny command',
+    function: helpCmd,
+    alises: []
+  }
+}
+
+function helpCmd(msg, ...params) {
+  const cmd = params[0]
+
+  const embed = new DiscordJS.MessageEmbed()
+    .setTitle(cmd.touppercase())
+
+}
+
+const client = new DiscordJS.Client( {
   intents: [
-    
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
   ]
+} )
+
+client.on("ready", () => {
+  console.log("READDYYYYYYYY");
+})
+
+client.on("messageCreated", msg => {
+  const [starter, ...params] = msg.split(' ')
+
+  const prefix = starter.substr(0, 1);
+  const cmd = starter.substr(1)
+
+  if (prefix == config.prefix) {
+
+    cmds[cmd].function(msg, ...params)
+  }  
 })
